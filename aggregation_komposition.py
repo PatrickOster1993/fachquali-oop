561d970bf7136c11736f70d4b9b84d502f11317e
Patrick Oster <p.oster9330@googlemail.com>
2025-04-03 09:13:03 +0200

    optimized aggregation- u. kompositionsvergleich

--- aggregation_komposition.py
+++ aggregation_komposition.py
@@ -49,12 +49,15 @@ class Motor:
 mein_auto = Auto()
 mein_auto.fahren()
 
+# Merksatz 0: Stellen Sie sich den Zusammenhang zwischen "Teil" und "Ganzem" bitte NICHT
+# als "Reale Welt Beziehung" vor! Die Software hat ihre ganz eigenen Gesetzmäßigkeiten!
+# Sie können aber den lebensweltlichen Gedanken IM NACHGANG mit der technischen Realisierung
+# vergleichen --> das schult die konkreten Unterschiede und das Wissen um die softwareseitige
+# Realisierung!
+
 # Merksatz 1: Wird das "Teil" (hier Motor) in irgendeiner Weise extern dem "Ganzen" (hier Autor) übergeben,
 # so MUSS davon ausgegangen werden, dass bereits eine Instanz des "Teils" unabhängig vom "Ganzen" erzeugt wurde.
 # Hier läge dann immer eine Aggregationsbeziehung vor - ansonsten eine Kompositionsbeziehung!
 
 # Merksatz 2: Eine Aggregation liegt immer dann vor, wenn die Existenz eines "Teils" in ALLEN Fällen
 # unabhängig von der Existenz des "Ganzen" ist!
-
-
-
