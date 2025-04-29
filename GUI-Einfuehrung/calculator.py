b8f205156393ba9259633eb50c9b2b8ded7e9358
unknown <LightningXZero@gmx.de>
2025-04-29 11:06:29 +0200

    mitgeschrieben

--- GUI-Einfuehrung/calculator.py
+++ GUI-Einfuehrung/calculator.py
@@ -1,43 +1,39 @@
-# sys Wird benötigt, um Programm ordentlich zu beenden
+# Wird benötigt um Program ordentlich zu beenden
 import sys
 
-# Widgets, die wir benötigen
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
-# QApplication: Erstellen einer Instanz ihrer Applikation
-# QWidget: Basisklasse (= Elternklasse), die etwaige, für das Fenster erforderliche Attribute, Methoden, ... besitzt.
-# QVBoxLayout: Erstellt ein vertikales Layout, um grafische Elemente (= Widgets) untereinander anzuordnen / stacken
+# Widgets die wir benötigen
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QGridLayout
 
 class Calculator(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.running = True
-        self.display = ""
         self.initUI()
-    
+
     def initUI(self):
-        print("Mein Taschenrechner")
-        print("###################")
+        print("Mein Taschenrecher")
+        print("##################")
+
+        vbox = QVBoxLayout()
+
+        self.display = QLineEdit()
+        vbox.addWidget(self.display) # einzelnes graphisches Element ( Texteingabefeld )
+
+        grid = QGridLayout()
+
         
 
-    def onButtonClicked(self):
-        text = self.display.lower()
-        if text == 'exit':
-            print("Taschenrechner wird beendet!")
-            self.running = False
-        elif text == 'c':
-            print("Bildschirm bereinigt!")
-            self.display = ""
-        elif text == '=':
-            print("Nichts zu berechnen. Bitte Berechnung eingeben!")
-        else:
-            result = round(float(eval(text)), 8)
-            print(f"Ergebnis: {result}")
+        self.setLayout(vbox) # Hauptlayout auf Fenster anwenden
+        self.setWindowTitle("Taschenrechner")
 
-app = QApplication(sys.argv) # sys.argv -> ordentliches Beenden (Integration von GUI in "Systemlandschaft") (*)
+    
+    def onButtonClicked(self):
+        pass
+
+app = QApplication(sys.argv)
 my_calculator = Calculator()
 
 my_calculator.show()
 
-sys.exit(app.exec_()) # (*) + initiales Starten der Ereignisschleife für Benutzerinteraktion.
+sys.exit(app.exec_())
 
