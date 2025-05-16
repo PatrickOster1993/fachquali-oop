3f3f6f5a2fe0a74d584f7af715a74fda0cc1a20a
Patrick Oster <p.oster9330@googlemail.com>
2025-05-16 12:57:09 +0200

    added integrationstest

--- softwaretesting/tests/kreditkarte_test.py
+++ softwaretesting/tests/kreditkarte_test.py
@@ -2,3 +2,32 @@
 # bezahlen(self, betrag)
 
 # Your turn!
+
+import unittest
+from bankkonto import Bankkonto
+from kreditkarte import Kreditkarte
+
+class TestKreditkarte(unittest.TestCase):
+    
+    def setUp(self):
+        self.konto = Bankkonto("Testuser", 1000)
+        self.karte = Kreditkarte(self.konto.getInhaber(), self.konto, 10000)
+
+    def testLimitNegativBeiInit(self):
+        with self.assertRaises(ValueError):
+            Kreditkarte(self.konto.getInhaber(), self.konto, -100)
+    
+    def testBetragNegativ(self):
+        with self.assertRaises(ValueError):
+            self.karte.bezahlen(-50)
+    
+    def testBetragUeberLimit(self):
+        erfolgreich = self.karte.bezahlen(2000)
+        self.assertFalse(erfolgreich)
+    
+    def testBezahlenErfolgreich(self):
+        erfolgreich = self.karte.bezahlen(500)
+        self.assertTrue(erfolgreich)
+
+    def testInhaberIdentisch(self):
+        self.assertEqual(self.karte.getInhaber(), self.konto.getInhaber())
